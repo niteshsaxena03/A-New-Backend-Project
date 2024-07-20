f5c233d34d8d57b497dce3ccd72988d8885429c8
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-20 14:35:56 +0530

    Doing some configuration in app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-const app=express();
+const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    Credential:true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true,
+  })
+);
 
-export {app};
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+export { app };
