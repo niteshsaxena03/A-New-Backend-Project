f61c73dcdf2434a6910db4825805690fa135fe4b
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-20 17:32:50 +0530

    Creating a custom class for response

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+  constructor(statusCode, message = "Success", data) {
+    this.message = message;
+    this.success = statusCode < 400;
+    this.data = data;
+    this.statusCode = statusCode;
+  }
+}
