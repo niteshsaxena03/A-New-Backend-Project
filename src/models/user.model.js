61992f973a5a55c33abdd884391b165e23f6b433
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-20 18:13:29 +0530

    Creating a function to check whether password is correct or not

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
@@ -53,5 +53,9 @@ userSchema.pre("save", async function (next) {
   this.password = bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
