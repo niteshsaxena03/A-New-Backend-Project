7dd922f3977974664feb46cfa20293780cad39c3
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-20 17:51:33 +0530

    Creating user model

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`Mongo connected ${connectionInstance.connection.host}`);
+    console.log(`MongoDB connected ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("connection error", error);
     process.exit(1);
