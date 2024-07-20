df7b15422bb5bde452173e15073e304d35789f50
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-20 18:08:55 +0530

    Using bcrypt to hash the password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     userName: {
@@ -40,11 +41,17 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required"],
     },
-    refreshToken:{
-        type:String,
-    }
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
