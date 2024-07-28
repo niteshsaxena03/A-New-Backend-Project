3064ca852df35f35b881a378f5a2042a06470c29
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-28 11:22:42 +0530

    checking if user already exists

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   const { fullName, userName, email, password } = req.body;
   console.log(email);
-  
+
   //validate--not empty
   if (
     [fullName, email, userName, password].some((field) => field?.trim() === "")
@@ -13,6 +14,12 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are compulsory");
   }
   //check if user already exist
+  const existingUser = User.findOne({
+    $or: [{ userName }, { email }],
+  });
+  if (existingUser) {
+    throw new ApiError(409, "Username or email already exists");
+  }
   //check for images-->upload to cloudinary
   //create user object-->entry in db
   //remove password and refresh token from response
