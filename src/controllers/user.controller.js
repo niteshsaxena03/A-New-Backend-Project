580d9dfd016cc63a6888f32a8e167381312cd576
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-28 11:19:00 +0530

    Adding a validation check for incoming data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   const { fullName, userName, email, password } = req.body;
   console.log(email);
+  
   //validate--not empty
+  if (
+    [fullName, email, userName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are compulsory");
+  }
   //check if user already exist
   //check for images-->upload to cloudinary
   //create user object-->entry in db
