001a79b51516782c7253f19c8170a01b09925109
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-08-04 20:08:30 +0530

    Added a new function to user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -211,4 +211,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
